9e798923f16a79481cd6e6aaeaa14fe0f3e69a93
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-29 17:54:36 +0900

    part1 introduction welcome to svelte

--- apps/part1-introduction/src/App.svelte
+++ apps/part1-introduction/src/App.svelte
@@ -1,30 +1,15 @@
 <script lang="ts">
-	export let name: string;
+import Links from './Links.svelte';
+import WelcomeToSvelte from './WelcomeToSvelte/App.svelte';
+import Router from 'svelte-spa-router';
+
+const routes = {
+  '/': WelcomeToSvelte,
+  '/welcome-to-svelte': WelcomeToSvelte,
+};
 </script>
 
 <main>
-	<h1>Welcome {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Links />
+  <Router {routes} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ apps/part1-introduction/src/Links.svelte
@@ -0,0 +1,8 @@
+<style>
+a {
+  display: block;
+}
+</style>
+
+<a href="#/">Home</a>
+<a href="#/welcome-to-svelte">Welcome to svelte</a>
