16d5ad090138a40488c6830e769f5a75bf0093c3
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-29 17:58:44 +0900

    part1 introduction your first component

--- apps/part1-introduction/src/App.svelte
+++ apps/part1-introduction/src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 import Links from './Links.svelte';
 import WelcomeToSvelte from './1. Welcome to Svelte/App.svelte';
+import YourFirstComponent from './2. Your first component/App.svelte';
 import Router from 'svelte-spa-router';
 
 const routes = {
   '/': WelcomeToSvelte,
   '/welcome-to-svelte': WelcomeToSvelte,
+  '/your-first-component': YourFirstComponent,
 };
 </script>
 
--- apps/part1-introduction/src/Links.svelte
+++ apps/part1-introduction/src/Links.svelte
@@ -4,5 +4,7 @@ a {
 }
 </style>
 
+<h1>Part1. Introduction</h1>
 <a href="#/">Home</a>
 <a href="#/welcome-to-svelte">Welcome to svelte</a>
+<a href="#/your-first-component">Your first component</a>
