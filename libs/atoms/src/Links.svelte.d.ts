a5ff107201055832940160165f1304d512e34e42
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-30 12:34:25 +0900

    remove Link.svelte.d.ts

--- libs/atoms/src/Links.svelte.d.ts
+++
@@ -1,7 +0,0 @@
-import { SvelteComponent } from 'svelte';
-
-export class Links extends SvelteComponent<{
-  headTitle: string;
-  titles: Record<string, string>;
-  routes: Record<string, ComponentType>;
-}> {}
