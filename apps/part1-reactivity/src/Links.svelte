aa7d4e3e932b300474ba7363570c556166d1918c
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-30 09:39:08 +0900

    part1 reactivity links change sort order

--- apps/part1-reactivity/src/Links.svelte
+++ apps/part1-reactivity/src/Links.svelte
@@ -1,3 +1,12 @@
+<section class="links">
+  <h1>Part1. Reactivity</h1>
+  <a href="#/">Home</a>
+  <a href="#/assignments">Assignments</a>
+  <a href="#/declarations">Declarations</a>
+  <a href="#/statements">Statements</a>
+  <a href="#/updating-arrays-and-objects">Updating arrays and objects</a>
+</section>
+
 <style>
   a {
     display: block;
@@ -8,12 +17,3 @@
     margin: 0 0 10px 0;
   }
 </style>
-
-<section class="links">
-  <h1>Part1. Reactivity</h1>
-  <a href="#/">Home</a>
-  <a href="#/assignments">Assignments</a>
-  <a href="#/declarations">Declarations</a>
-  <a href="#/statements">Statements</a>
-  <a href="#/updating-arrays-and-objects">Updating arrays and objects</a>
-</section>
