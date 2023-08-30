f8318b12d476ab43fdb857895dec5899f882845f
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-30 09:57:19 +0900

    part1 props spread props

---
+++ apps/part1-props/src/3. Spread props/PackageInfo.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { PackageInfoType } from './PackageInfoType';
+
+  let packageInfo: PackageInfoType = {};
+
+  export let name = packageInfo.name;
+  export let version = packageInfo.version;
+  export let speed = packageInfo.speed;
+  export let website = packageInfo.website;
+
+  $: href = `https://www.npmjs.com/package/${name}`;
+</script>
+
+<p>
+  The <code>{name}</code> package is {speed} fast. Download version {version} from
+  <a {href}>npm</a>
+  and <a href="{website}">learn more here</a>.
+</p>
