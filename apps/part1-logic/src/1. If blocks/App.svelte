e4548d8ee4fab5080c3385365b76078b9e11d3c0
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-30 12:42:32 +0900

    part1 logic if blocks

---
+++ apps/part1-logic/src/1. If blocks/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let count = 0;
+
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click="{increment}">
+  Clicked {count}
+  {count === 1 ? 'time' : 'times'}
+</button>
+
+{#if count > 10}
+  <p>{count} is greater than 10</p>
+{/if}
