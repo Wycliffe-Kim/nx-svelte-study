a6b958137b728689b180824e697d8f2b246ad14d
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-30 12:40:26 +0900

    part1 logic setup

---
+++ apps/part1-logic-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,5 @@
+describe('part1-logic', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {});
+});
---
+++ apps/part1-logic/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { Links } from '@nx-svelte-study/atoms';
+  import IfBlocks from './1. If blocks/App.svelte';
+  import ElseBlocks from './2. Else blocks/App.svelte';
+  import ElseIfBlocks from './3. Else-if blocks/App.svelte';
+  import EachBlocks from './4. Each blocks/App.svelte';
+  import KeyedEachBlocks from './5. Keyed each blocks/App.svelte';
+  import AwaitBlocks from './6. Await blocks/App.svelte';
+</script>
+
+<Links
+  headTitle="Part 1: Logic"
+  titles="{{
+    '/': 'Home',
+    '/if-blocks': 'If blocks',
+    '/else-blocks': 'Else blocks',
+    '/else-if-blocks': 'Else-if blocks',
+    '/each-blocks': 'Each blocks',
+    '/keyed-each-blocks': 'Keyed each blocks',
+    '/await-blocks': 'Await blocks',
+  }}"
+  routes="{{
+    '/': IfBlocks,
+    '/if-blocks': IfBlocks,
+    '/else-blocks': ElseBlocks,
+    '/else-if-blocks': ElseIfBlocks,
+    '/each-blocks': EachBlocks,
+    '/keyed-each-blocks': KeyedEachBlocks,
+    '/await-blocks': AwaitBlocks,
+  }}"
+/>
