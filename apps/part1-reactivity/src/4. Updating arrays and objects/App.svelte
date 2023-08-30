efa6c3d815ba3012a61c7b31003944ba7c2d1d62
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-30 09:35:52 +0900

    part1 reactivity updating arrays and objects

---
+++ apps/part1-reactivity/src/4. Updating arrays and objects/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { concat, range, reduce } from 'lodash';
+
+  let numbers = range(1, 4);
+
+  const addNumber = () => {
+    numbers = concat(numbers, numbers.length + 1);
+  };
+
+  $: sum = reduce(numbers, (total, currentNumber) => total + currentNumber, 0);
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click="{addNumber}"> Add a number </button>
