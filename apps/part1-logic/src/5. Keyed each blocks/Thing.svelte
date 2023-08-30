cfde5c5f90e976b00f3dbb6ed6569392bb5a879e
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-08-30 13:10:33 +0900

    part1 logic keyed each blocks

---
+++ apps/part1-logic/src/5. Keyed each blocks/Thing.svelte
@@ -0,0 +1,19 @@
+<script>
+  const emojis = {
+    apple: '🍎',
+    banana: '🍌',
+    carrot: '🥕',
+    doughnut: '🍩',
+    egg: '🥚',
+  };
+
+  // the name is updated whenever the prop value changes...
+  export let name;
+
+  // ...but the "emoji" variable is fixed upon initialisation
+  // of the component because it uses `const` instead of `$:`
+  // 만약에 thing.id를 사용하고 싶지 않다면 const 대신 $:를 사용하면 된다.
+  const emoji = emojis[name];
+</script>
+
+<p>{emoji} = {name}</p>
