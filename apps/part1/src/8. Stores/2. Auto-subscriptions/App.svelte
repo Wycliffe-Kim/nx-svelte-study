8a0d98ca95b42e9c7a123cfb0398508002b5d703
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-05 15:25:47 +0900

    part1 stores auto-subscriptions

---
+++ apps/part1/src/8. Stores/2. Auto-subscriptions/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { count } from './stores';
+  import Incrementer from './Incrementer.svelte';
+  import Decrementer from './Decrementer.svelte';
+  import Resetter from './Resetter.svelte';
+  // import { onDestroy } from 'svelte';
+
+  // let countValue;
+
+  // const unsubscribe = count.subscribe((value) => {
+  //   countValue = value;
+  // });
+
+  // onDestroy(unsubscribe);
+</script>
+
+<h1>The count is {$count}</h1>
+<!-- <h1>The count is {countValue}</h1> -->
+
+<Incrementer />
+<Decrementer />
+<Resetter />
