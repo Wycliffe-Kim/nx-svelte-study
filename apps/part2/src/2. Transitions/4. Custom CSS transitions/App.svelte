62d6d388ceb3c8f4addcd08e2fefb62505f7bd15
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-05 17:29:24 +0900

    part2 transitions custom css transitions

---
+++ apps/part2/src/2. Transitions/4. Custom CSS transitions/App.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { FadeParams, fade } from 'svelte/transition';
+  import { elasticOut } from 'svelte/easing';
+
+  let visible = true;
+
+  const spin = (_: Element, { duration }: FadeParams) => {
+    return {
+      duration,
+      css: (t) => {
+        const eased = elasticOut(t);
+
+        return `
+        transform: scale(${eased}) rotate(${eased * 1080}deg);
+        color: hsl(
+          ${Math.trunc(t * 360)},
+          ${Math.min(100, 1000 * (1 - t))}%,
+          ${Math.min(50, 500 * (1 - t))}%
+        );`;
+      },
+    };
+  };
+</script>
+
+<label>
+  <input type="checkbox" bind:checked="{visible}" />
+  visible
+</label>
+
+{#if visible}
+  <div class="centered" in:spin="{{ duration: 8000 }}" out:fade>
+    <span>transitions!</span>
+  </div>
+{/if}
+
+<style>
+  .centered {
+    position: absolute;
+    left: 15%;
+    bottom: 30%;
+    transform: translate(-50%, -50%);
+  }
+
+  span {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    font-size: 4em;
+  }
+</style>
