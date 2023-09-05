dad199ee7ebcad2fb718f18442cf1dbd1e695b52
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-05 12:25:38 +0900

    part1 stores setup

---
+++ apps/part1-stores-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,5 @@
+describe('part1-stores', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {});
+});
---
+++ apps/part1-stores/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { Links } from '@nx-svelte-study/atoms';
+  import WritableStores from './1. Writable stores/App.svelte';
+  import AutoSubscriptions from './2. Auto-subscriptions/App.svelte';
+  import ReadableStores from './3. Readable stores/App.svelte';
+  import DerivedStores from './4. Derived stores/App.svelte';
+  import CustomStores from './5. Custom stores/App.svelte';
+  import StoreBindings from './6. Store bindings/App.svelte';
+</script>
+
+<Links
+  headTitle="Part 1: Stores"
+  titles="{{
+    '/': 'Home',
+    '/writable-stores': 'Writable stores',
+    '/auto-subscriptions': 'Auto-subscriptions',
+    '/readable-stores': 'Readable stores',
+    '/derived-stores': 'Derived stores',
+    '/custom-stores': 'Custom stores',
+    '/store-bindings': 'Store bindings',
+  }}"
+  routes="{{
+    '/': WritableStores,
+    '/writable-stores': WritableStores,
+    '/auto-subscriptions': AutoSubscriptions,
+    '/readable-stores': ReadableStores,
+    '/derived-stores': DerivedStores,
+    '/custom-stores': CustomStores,
+    '/store-bindings': StoreBindings,
+  }}"
+/>
