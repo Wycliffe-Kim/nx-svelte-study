13560ac53bbc048f122e234e0355da24a6765492
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-05 20:05:35 +0900

    part2 transitions global transitions

---
+++ apps/part2/src/2. Transitions/7. Global transitions/App.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { slide } from 'svelte/transition';
+
+  let showItems = true;
+  let i = 5;
+  let items = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+  ];
+</script>
+
+<label>
+  <input type="checkbox" bind:checked="{showItems}" />
+  show list
+</label>
+
+<label>
+  <input type="range" bind:value="{i}" max="10" />
+</label>
+
+<section class="list">
+  <section class="content">
+    <p>Global</p>
+    {#if showItems}
+      {#each items.slice(0, i) as item}
+        <div transition:slide|global>
+          {item}
+        </div>
+      {/each}
+    {/if}
+  </section>
+  <section class="content">
+    <p>Local</p>
+    {#if showItems}
+      {#each items.slice(0, i) as item}
+        <div transition:slide>
+          {item}
+        </div>
+      {/each}
+    {/if}
+  </section>
+</section>
+
+<style>
+  div {
+    padding: 0.5em 0;
+    border-top: 1px solid #eee;
+  }
+
+  p {
+    font-weight: bold;
+    font-size: large;
+  }
+
+  .list {
+    display: flex;
+  }
+
+  .content {
+    width: 100px;
+    margin: 0 10px;
+  }
+</style>
