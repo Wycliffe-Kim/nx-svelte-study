fb434b05f40bd210e51392b447ce991e02a6d064
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-05 10:39:55 +0900

    part1 events dom event forwarding

---
+++ apps/part1-events/src/6. DOM event forwarding/DomEventForwarding.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import BigRedButton from './BigRedButton.svelte';
+
+  const handleClick = () => {
+    alert('Clicked!');
+  };
+</script>
+
+<BigRedButton on:click="{handleClick}" />
