5351c9b18c9253504509fbe04f91812bf33342c9
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-05 10:32:23 +0900

    part1 events component events

---
+++ apps/part1-events/src/4. Component events/ComponentEvents.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { EventData } from './EventData';
+  import Inner from './Inner.svelte';
+
+  const handleMessage = (event: CustomEvent<EventData>) => {
+    alert(event.detail.text);
+  };
+</script>
+
+<Inner on:message="{handleMessage}" />
