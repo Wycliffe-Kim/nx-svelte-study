321f01a00531d943c12296f8e5e13160d4a8d27d
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-06 12:59:46 +0900

    part2 actions adding parameters

---
+++ apps/part2/src/4. Actions/2. Adding parameters/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import tippy from 'tippy.js';
+  import 'tippy.js/dist/tippy.css';
+  import 'tippy.js/themes/material.css';
+
+  let content = 'Hello!';
+
+  const tooltip = (node: HTMLElement, options: tippy.Props) => {
+    const tooltip = tippy(node, options);
+
+    return {
+      update: (options: tippy.Props) => {
+        tooltip.setProps(options);
+      },
+      destroy: () => {
+        tooltip.destroy();
+      },
+    };
+  };
+</script>
+
+<input bind:value="{content}" />
+
+<button use:tooltip="{{ content, theme: 'material' }}"> Hover me </button>
