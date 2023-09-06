216c984b6235a31b883ed278ebefe66f0eb76242
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-09-06 09:27:56 +0900

    part2 animations the animate directive

---
+++ apps/part2/src/3. Animations/1. The animate directive/TodoList.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { send, receive } from './transition';
+  import { flip } from 'svelte/animate';
+  import { TodoStore } from './todos';
+
+  export let store: TodoStore;
+  export let done: boolean;
+</script>
+
+<ul class="todos">
+  {#each $store.filter((todo) => todo.done === done) as todo (todo.id)}
+    <li
+      class:done
+      in:receive="{{ key: todo.id }}"
+      out:send="{{ key: todo.id }}"
+      animate:flip="{{ duration: 250 }}"
+    >
+      <label>
+        <input
+          type="checkbox"
+          checked="{todo.done}"
+          on:change="{(e) => store.mark(todo, e.currentTarget.checked)}"
+        />
+
+        <span>{todo.description}</span>
+
+        <button on:click="{() => store.remove(todo)}" aria-label="Remove"
+        ></button>
+      </label>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  label {
+    width: 100%;
+    height: 100%;
+    display: flex;
+  }
+
+  span {
+    flex: 1;
+  }
+
+  button {
+    background-image: url(./remove.svg);
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  .done {
+    text-decoration: line-through;
+  }
+</style>
